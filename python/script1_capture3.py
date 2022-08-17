c5b0a2a001382b7cebbd73a2c9c747a4bc82251b
Davide Giacomini <giacomini.davide@outlook.com>
2022-08-17 17:58:29 +0200

    Created capture3_train.csv

---
+++ python/script1_capture3.py
@@ -0,0 +1,34 @@
+## THIS SCRIPT CONVERTS ALL THE PACKETS INTO AN ARRAY DIVIDED IN INBOUND AND OUTBOUND
+## IT IS VERY LONG SO IT'S ADVICED TO HAVE IT RUN ON A PYTHON CONSOLE TO SAVE THE ARRAYS AND THEN USE THE OTHER SCRIPTS
+
+
+# Needed to run tshark on colab
+import os
+import nest_asyncio
+
+import pyshark
+
+# Useful for treating table-like data
+import pandas as pd
+# Numerical operations library
+import numpy as np
+
+nest_asyncio.apply()
+
+path = "capture3_train.pcapng"
+
+packets = pyshark.FileCapture(path, display_filter='ip.addr == 149.137.10.223 && ip.addr == 192.168.1.15 && udp.port == 8801 && udp.port == 52409')
+
+PACKETS = []
+
+count = 0
+for packet in packets:
+  try:
+    PACKETS.append(packet)
+    count+=1
+    if count%1000 == 0:
+        print(count)
+  except:
+    print("some error")
+
+print(len(PACKETS))
--- python/script3_capture3.py
+++ python/script3_capture3.py
@@ -28,10 +28,10 @@ def in_out_pkts_number(packets):
 def person_or_background(packets):
     first_time = float(packets[0].udp.time_relative)
     last_time = float(packets[-1].udp.time_relative)
-    FIRST_THRESHOLD = 139.0
-    SECOND_THRESHOLD = 210.0
-    THIRD_THRESHOLD = 279.0
-    FOURTH_THRESHOLD = 353.0
+    FIRST_THRESHOLD = 66.0
+    SECOND_THRESHOLD = 133.0
+    THIRD_THRESHOLD = 223.0
+    FOURTH_THRESHOLD = 285.0
 
     if last_time < FIRST_THRESHOLD or first_time >= FOURTH_THRESHOLD or (first_time >= SECOND_THRESHOLD and last_time < THIRD_THRESHOLD):
         return 1
